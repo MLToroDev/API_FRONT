563e7fed9c25831791689c5b29fb870e6714de65
Laptop Mateo <realmateox22@gmail.com>
2022-11-07 14:07:15 -0500

    cambios

--- Script/informer.js
+++ Script/informer.js
@@ -7,16 +7,28 @@ $(document).ready(() => {
 });
 
 $('#btnBuscar').click(() => {
+  
+  let t = $("tbody");
+  t.empty();
+  const Codigo = $('#codigo');
+  let p = Codigo.val()
 
-    const Codigo = $('#id');
-   
-    $.get("http://localhost:3000/api/producto/"+Codigo.val(), (rs) => {
-        console.log(rs);
-        alert("registro success");
-           cargarTabla();
+  console.log(p);
+
+
+  $.get("http://localhost:3000/api/producto/buscar/" , p, (rs) => {
+    rs.forEach(Producto => {
+      t.append(`<tr>
+  <td>${Producto.Codigo}</td>
+  <td>${Producto.Nombre}</td>
+  <td>${Producto.Cantidad}</td>
+  <td>${Producto.ValorU}</td>
+  <td>${Producto.PrecioV}</td>
+  <td>${Producto.Cliente}</td>
+  </tr>`);
+
+
+    })
+    console.log(rs);
   })
-    
-
-
-
 });
